899d460e6c6fc29c57cd5c68ecefd933ebf9e910
Richard Pawson <rpawson@nakedobjects.net>
2023-09-20 16:00:48 +0100

    #14 tests written

---
+++ Test/CompilerTests/T14_Lists.cs
@@ -0,0 +1,437 @@
+﻿using Compiler;
+
+namespace Test.CompilerTests;
+
+using static Helpers;
+
+[TestClass]
+public class T14_Lists {
+
+    #region Passes
+    [TestMethod]
+    public void Pass_literalList() {
+        var code = @"
+main
+    var a = {4,5,6,7,8}
+    printLine(a)
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+    var a = new ImmutableList<int> {4,5,6,7,8};
+    printLine(a);
+  }
+}";
+
+        var parseTree = @"(file (main main (statementBlock (callStatement (expression (methodCall printLine ( (argumentList (expression (value (literal (literalDataStructure ""Hello World!""))))) ))))) end main) <EOF>)";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "{4,5,6,7,8}\r\n");
+    }
+
+    [TestMethod]
+    public void Pass_literalListWithCoercion()
+    {
+        var code = @"
+main
+    var a = {4.1,5,6,7,8}
+    printLine(a)
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+    var a = new ImmutableList<double> {4.1,5,6,7,8};
+    printLine(a);
+  }
+}";
+
+        var parseTree = @"(file (main main (statementBlock (callStatement (expression (methodCall printLine ( (argumentList (expression (value (literal (literalDataStructure ""Hello World!""))))) ))))) end main) <EOF>)";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "{4.1,5,6,7,8}\r\n");
+    }
+
+    [TestMethod]
+    public void Pass_length()
+    {
+        var code = @"
+main
+    var a = {4,5,6,7,8}
+    printLine(a.length())
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+    var a = new ImmutableList<int> {4,5,6,7,8};
+    printLine(a.length());
+  }
+}";
+
+        var parseTree = @"(file (main main (statementBlock (callStatement (expression (methodCall printLine ( (argumentList (expression (value (literal (literalDataStructure ""Hello World!""))))) ))))) end main) <EOF>)";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "5\r\n");
+    }
+
+
+    [TestMethod]
+    public void Pass_emptyList()
+    {
+        var code = @"
+main
+    var a = new List<Int>()
+    printLine(a.length())
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+    var a = new ImmutableList<int>();
+    printLine(a.length);
+    printLine(a);
+  }
+}";
+
+        var parseTree = @"(file (main main (statementBlock (callStatement (expression (methodCall printLine ( (argumentList (expression (value (literal (literalDataStructure ""Hello World!""))))) ))))) end main) <EOF>)";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "0\r\n{}\r\n");
+    }
+
+    [TestMethod]
+    public void Pass_index()
+    {
+        var code = @"
+main
+    var a = {4,5,6,7,8}
+    printLine(a[2])
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+   private static void Main(string[] args) {
+    var a = new ImmutableList<int> {4,5,6,7,8};
+    printLine(a[2]);
+  }
+}";
+
+        var parseTree = @"";
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "6\r\n");
+    }
+
+    [TestMethod]
+    public void Pass_addMethod()
+    {
+        var code = @"
+main
+    var a = {4,5,6,7,8}
+    var b = a.add(9)
+    printLine(a)
+    printLine(b)
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+   private static void Main(string[] args) {
+    var a = new ImmutableList<int> {4,5,6,7,8};
+    var b = a.Add(9);
+    printLine(a);
+    printLine(b);
+  }
+}";  
+
+        var parseTree = @"(file (main main (statementBlock (callStatement (expression (methodCall printLine ( (argumentList (expression (value (literal (literalDataStructure ""Hello World!""))))) ))))) end main) <EOF>)";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "{4,5,6,7,8,9}\r\n");
+    }
+
+    [TestMethod]
+    public void Pass_addValueUsingPlus()
+    {
+        var code = @"
+main
+    var a = {4,5,6,7,8}
+    var b = a + 9
+    printLine(a)
+    printLine(b)
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+   private static void Main(string[] args) {
+    var a = new ImmutableList<int> {4,5,6,7,8};
+    var b = a.Add(9);
+    printLine(a);
+    printLine(b);
+  }
+}";
+
+        var parseTree = @"(file (main main (statementBlock (callStatement (expression (methodCall printLine ( (argumentList (expression (value (literal (literalDataStructure ""Hello World!""))))) ))))) end main) <EOF>)";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "{4,5,6,7,8,9}\r\n");
+    }
+
+    [TestMethod]
+    public void Pass_addListToElement()
+    {
+        var code = @"
+main
+    var a = {4,5,6,7,8}
+    var b = 9 + a
+    printLine(a)
+    printLine(b)
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+   private static void Main(string[] args) {
+    var a = new ImmutableList<int> {4,5,6,7,8};
+    var b = a.Add(9);
+    printLine(a);
+    printLine(b);
+  }
+}";
+
+        var parseTree = @"(file (main main (statementBlock (callStatement (expression (methodCall printLine ( (argumentList (expression (value (literal (literalDataStructure ""Hello World!""))))) ))))) end main) <EOF>)";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "{9,4,5,6,7,8}\r\n");
+    }
+
+    [TestMethod]
+    public void Pass_addListToListUsingPlus()
+    {
+        var code = @"
+main
+    var a = {4,5,6,7,8}
+    var b = {1,2,3}
+    var c = a + b
+    printLine(c)
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+   private static void Main(string[] args) {
+    var a = new ImmutableList<int> {4,5,6,7,8};
+    var b = new ImmutableList<int> {1,2,3};
+    var c = a.AddRange(b);
+    printLine(c);
+  }
+}";
+
+        var parseTree = @"(file (main main (statementBlock (callStatement (expression (methodCall printLine ( (argumentList (expression (value (literal (literalDataStructure ""Hello World!""))))) ))))) end main) <EOF>)";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "{4,5,6,7,8,1,2,3}\r\n");
+    }
+#endregion
+
+    #region Fails
+    [TestMethod]
+    public void Fail_emptyLiteralList()
+    {
+        var code = @"
+main
+    var a = {}
+end main
+";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertDoesNotParse(compileData);
+    }
+
+    [TestMethod]
+    public void Fail_literalListInconsistentTypes1()
+    {
+        var code = @"
+main
+    var a = {3, ""apples""}
+end main
+";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertDoesNotParse(compileData);
+    }
+
+    [TestMethod]
+    public void Fail_literalListInconsistentTypes2()
+    {
+        var code = @"
+main
+    var a = {3, 3.1}
+end main
+";  //Because list type is decided by FIRST element
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertDoesNotParse(compileData);
+    }
+
+    [TestMethod]
+    public void Fail_OutOfRange()
+    {
+        var code = @"
+main
+    var a = {4,5,6,7,8}
+    var b = a[5];
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+   private static void Main(string[] args) {
+    var a = new ImmutableList<int> {4,5,6,7,8};
+    var b = a[5];
+  }
+}";
+
+        var parseTree = @"";
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "out of range error");
+    }
+    #endregion
+}
