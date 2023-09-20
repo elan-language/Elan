f806a45245ec8cb07a5de3ae86a0442b1cb11087
Richard Pawson <rpawson@nakedobjects.net>
2023-09-20 13:20:33 +0100

    #12 tests created

---
+++ Test/CompilerTests/T12_Arrays.cs
@@ -0,0 +1,182 @@
+﻿using Compiler;
+
+namespace Test.CompilerTests;
+
+using static Helpers;
+
+[TestClass, Ignore]
+public class T12_Arrays
+{
+    [TestMethod]
+    public void Pass_DeclareAnEmptyArrayBySizeAdCheckLength()
+    {
+        var code = @"
+main
+    var a = new Array<String>(3)
+    printLine(a.length())
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+    var a = new string[3];
+    printLine(a.length());
+  }
+}";
+
+        var parseTree = @"*";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "3\r\n");
+    }
+
+    [TestMethod]
+    public void Pass_ConfirmStringElementsInitializedToEmptyStringNotNull()
+    {
+        var code = @"
+main
+    var a = new Array<String>(3)
+    printLine(a[0].length())
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+    var a = new string[3];
+    printLine(a.length());
+  }
+}";
+
+        var parseTree = @"*";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "0\r\n");
+    }
+
+    [TestMethod]
+    public void Pass_SetAndReadElements()
+    {
+        var code = @"
+main
+    var a = new Array<String>(3)
+    a[0] = ""foo""
+    a[2] = ""yon""
+    printLine(a[0])
+    printLine(a[2])
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+    var a = new string[3];
+    a[0] = ""foo"";
+    a[2] = ""yon"";
+    printLine(a[0]);
+    printLine(a[2]);
+  }
+}";
+
+        var parseTree = @"*";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "foo\r\nyon\r\n");
+    }
+
+    [TestMethod]
+    public void Fail_OutOfRange()
+    {
+        var code = @"
+main
+    var a = new Array<String>(3)
+    var b = a[3]
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static GlobalConstants;
+using static StandardLibrary.SystemCalls;
+
+public static partial class GlobalConstants {
+
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+    var a = new string[3];
+    var b = a[3];
+  }
+}";
+
+        var parseTree = @"*";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "Out Of Range"); //or something to that effect
+    }
+
+
+    [TestMethod]
+    public void Fail_TypeIncompatibility()
+    {
+        var code = @"
+main
+    var a = new Array<String>(3)
+    a[0] = true
+end main
+";
+
+        var parseTree = @"*";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertDoesNotCompile(compileData);
+    }
+}
