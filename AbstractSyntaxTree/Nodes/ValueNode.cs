1350533c090007ff808cab5cca1a4ba7e4c7e154
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-09-25 16:22:32 +0100

    #30 fix compile error

--- AbstractSyntaxTree/Nodes/ValueNode.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace AbstractSyntaxTree.Nodes;
-
-public record ValueNode(string Value, ISymbolType SymbolType) : IScalarValueNode {
-    public IEnumerable<IAstNode> Children => Array.Empty<IAstNode>();
-}
