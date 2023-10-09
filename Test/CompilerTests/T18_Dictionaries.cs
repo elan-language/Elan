6c69f6de0effa3080ab3f91220e807a0dc40b3b7
Richard Pawson <rpawson@nakedobjects.net>
2023-10-09 08:51:33 +0100

    #T18 - added tests - but incomplete

---
+++ Test/CompilerTests/T18_Dictionaries.cs
@@ -0,0 +1,89 @@
+﻿using Compiler;
+
+namespace Test.CompilerTests;
+
+using static Helpers;
+
+[TestClass, Ignore]
+public class T18_Dictionaries
+{
+    #region Passes
+
+    [TestMethod]
+    public void Pass_LiteralConstant() {
+        var code = @"#
+constant a = {'a':1, 'b':3, 'z':10}
+main
+  printLine(a)
+  printLine(a['z'])
+end main
+";
+
+        var objectCode = @"";
+
+        var parseTree = @"";
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "{'a':1, 'b':3, 'z':10}\r\n10\r\n");
+    }
+
+    [TestMethod]
+    public void Pass_ChangeEntry()
+    {
+        var code = @"#
+constant a = {'a':1, 'b':3, 'z':10}
+main
+  a['b'] = 4
+  printLine(a)
+end main
+";
+
+        var objectCode = @"";
+
+        var parseTree = @"";
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "{'a':1, 'b':4, 'z':10}\r\n");
+    }
+
+    //TODO
+    // Add new item - testing that target is not mutated
+    // count
+    // contains(key)
+    #endregion
+
+    #region Fails
+    [TestMethod]
+    public void Fail_RepeatedKey()
+    {
+        var code = @"#
+constant a = {'a':1, 'b':3, 'a':10}
+main
+  printLine(a)
+end main
+";
+
+        var objectCode = @"";
+
+        var parseTree = @"";
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertDoesNotCompile(compileData);
+    }
+
+    //TODO
+    //key not present
+    //invalid type for key or value
+
+
+    #endregion
+}
