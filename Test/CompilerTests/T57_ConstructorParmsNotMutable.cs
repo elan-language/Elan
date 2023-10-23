85f5bb2c77661e32dca1eabecfa963f40f5482e4
Richard Pawson <rpawson@nakedobjects.net>
2023-10-23 11:58:14 +0100

    #57 2 tests written

---
+++ Test/CompilerTests/T57_ConstructorParmsNotMutable.cs
@@ -0,0 +1,62 @@
+﻿using Compiler;
+
+namespace Test.CompilerTests;
+
+using static Helpers;
+
+[TestClass, Ignore]
+public class T57_ConstructorParmsNotMutable
+{
+    #region Passes
+    #endregion
+
+    #region Fails
+
+    [TestMethod]
+    public void Fail_reassigningIntParam()
+    {
+        var code = @"#
+class Foo
+    constructor(p_1 Int)
+        p_1 = p1
+    end constructor
+
+    property p1 as Int
+
+    function asString() as String
+        return """"
+    end function
+
+end class
+";
+        var parseTree = @"*";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertDoesNotCompile(compileData);
+    }
+
+    [TestMethod]
+    public void Fail_MutatingArrayParam()
+    {
+        var code = @"#
+class Foo
+    constructor(a Array<Int>)
+        a[0] = 4
+    end constructor
+
+    function asString() as String
+        return """"
+    end function
+end class
+";
+        var parseTree = @"*";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertDoesNotCompile(compileData);
+    }
+    #endregion
+}
