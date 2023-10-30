98a1511e5c3fb417d6f180403e3da073eb327390
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-10-30 13:02:57 +0000

    #45 more tests

--- CSharpLanguageModel/Models/DefaultModel.cs
+++ CSharpLanguageModel/Models/DefaultModel.cs
@@ -4,6 +4,7 @@ namespace CSharpLanguageModel.Models;
 
 public record DefaultModel(ICodeModel Type, TypeType DefaultTypeType) : ICodeModel {
     public string ToString(int indent) => DefaultTypeType switch {
+        TypeType.Value when Type is ScalarValueModel { Value: "string" } => $@"""""",
         TypeType.Value => $@"default({Type})",
         TypeType.Class => $"{Type}.DefaultInstance",
         _ => throw new NotImplementedException()
