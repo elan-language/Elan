556d49a081b8c4ceabf51da1c2800a773bc61ebf
Richard Pawson <rpawson@nakedobjects.net>
2023-11-02 13:07:12 +0000

    #83 create test

---
+++ Test/CompilerTests/T83_PrintingCommonSymbols.cs
@@ -0,0 +1,54 @@
+﻿using Compiler;
+
+namespace Test.CompilerTests;
+
+using static Helpers;
+
+[TestClass]
+public class T83_PrintingCommonSymbols
+{
+    #region Passes
+
+
+    [TestMethod, Ignore]
+    public void Pass_CommonSymbolsAccessibleFromUKKeyboard()
+    {
+        var code = @"#
+main
+  printLine(""¬!£$%^&*()@~#`|<>'€"")
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static Globals;
+using static StandardLibrary.SystemCalls;
+using static StandardLibrary.Functions;
+using static StandardLibrary.Constants;
+
+public static partial class Globals {
+
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+    printLine(@$""¬!£$%^&*()@~#`|<>'€"");
+  }
+}";
+
+        var parseTree = @"*";
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "¬!£$%^&*()@~#`|<>'€\r\n");
+    }
+
+    #endregion
+
+    #region Fails
+
+    #endregion
+}
