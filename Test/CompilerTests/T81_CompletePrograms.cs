64c60f7c61e3c344c06f74793a2271873d1b0d16
Richard Pawson <rpawson@nakedobjects.net>
2023-11-06 15:57:28 +0000

    #81 QueueOfTiles test failing

--- Test/CompilerTests/T81_CompletePrograms.cs
+++ Test/CompilerTests/T81_CompletePrograms.cs
@@ -98,32 +98,7 @@ public static class Program {
 
     [TestMethod]
     public void Pass_MergeSort() {
-        var code = @"
-function mergeSort(list List<String>) -> List<String> 
-    var result = list
-    if list.length() > 1 then
-      var mid = list.length() div 2 
-      result = merge(mergeSort(list[..mid]), mergeSort(list[mid..]))
-    end if
-    return result
-end function
-
-function merge(a List<String>, b List<String>) -> List<String>
-    var result = List<String>()
-    if a.isEmpty() then 
-      result = b 
-    else if b.isEmpty() then
-      result = a
-    else if a[0].isBefore(b[0]) then 
-      result = a[0] + merge(a[1..], b) 
-    else 
-      result = b[0] + merge(a, b[1..])
-    end if
-    return result
-end function
-main
-end main
-";
+        var code = ReadElanSourceCodeFile("mergeSort.Elan");
 
         var objectCode = @"using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -161,7 +136,8 @@ public static partial class Globals {
 
 public static class Program {
   private static void Main(string[] args) {
-
+    var li = new StandardLibrary.ElanList<string>(@$""plum"", @$""apricot"", @$""lime"", @$""lemon"", @$""melon"", @$""apple"", @$""orange"", @$""strawberry"", @$""pear"", @$""banana"");
+    printLine(Globals.mergeSort(li));
   }
 }";
 
@@ -173,6 +149,113 @@ public static class Program {
         AssertCompiles(compileData);
         AssertObjectCodeIs(compileData, objectCode);
         AssertObjectCodeCompiles(compileData);
+
+    }
+
+    [TestMethod, Ignore]
+    public void Pass_QueueOfTiles()
+    {
+        var code = ReadElanSourceCodeFile("words_QueueOfTiles.elan");
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static Globals;
+using static StandardLibrary.SystemCalls;
+using static StandardLibrary.Functions;
+using static StandardLibrary.Constants;
+
+public static partial class Globals {
+  public const int startHandSize = 15;
+  public const int maxHandSize = 20;
+  public const int maxTilesPlayed = 50;
+  public const string letters = @$"" * ***ABCDEFGHIJKLMNOPQRSTUVWXYZ"";
+  public const string mainMenu = @$""
+========= 
+MAIN MENU
+=========
+
+1. Play game with random start hand,
+2. Play game with training start hand
+9. Quit"";
+  public static readonly StandardLibrary.ElanDictionary<char,int> tileDictionary = new StandardLibrary.ElanDictionary<char,int>(('A', 1), ('B', 2), ('C', 2), ('D', 2), ('E', 1), ('F', 3), ('G', 2), ('H', 3), ('I', 1), ('J', 5), ('K', 3), ('L', 2), ('M', 2), ('N', 1), ('O', 1), ('P', 2), ('Q', 5), ('R', 1), ('S', 1), ('T', 1), ('U', 2), ('V', 3), ('W', 3), ('X', 5), ('Y', 3), ('Z', 5));
+  public record class QueueOfTiles {
+    public static QueueOfTiles DefaultInstance { get; } = new QueueOfTiles._DefaultQueueOfTiles();
+    private QueueOfTiles() {}
+    public QueueOfTiles(int maxSize) {
+      this.maxSize = maxSize;
+      contents = new StandardLibrary.ElanArray<string>(maxSize);
+      rear = -1;
+    }
+    protected virtual StandardLibrary.ElanArray<string> contents { get; set; } = StandardLibrary.ElanArray<string>.DefaultInstance;
+    protected virtual int rear { get; set; } = default;
+    protected virtual int maxSize { get; set; } = default;
+    public virtual Boolean isNotEmpty() {
+
+      return rear != -1;
+    }
+    public virtual string show() {
+      var result = @$"""";
+      foreach (var letter in contents) {
+        result = result + letter;
+      }
+      return result + newline;
+    }
+    public virtual string asString() {
+
+      return StandardLibrary.Functions.typeAndProperties(this);
+    }
+    public virtual void initialise() {
+      rear = -1;
+      for (var count = 0; count <= maxSize - 2; count = count + 1) {
+        add();
+      }
+    }
+    public virtual void withdrawNextLetter(ref string letter) {
+      if (isNotEmpty()) {
+        letter = contents[0];
+        for (var count = 1; count <= rear; count = count + 1) {
+          contents[count - 1] = contents[count];
+          contents[rear] = @$"""";
+        }
+        rear = rear - 1;
+      }
+    }
+    public virtual void add() {
+      if (rear < maxSize - 1) {
+        rear = rear + 1;
+        var n = random(0, 30);
+        contents[rear] = asString(letters[n]);
+      }
+    }
+    private record class _DefaultQueueOfTiles : QueueOfTiles {
+      public _DefaultQueueOfTiles() { }
+      protected override StandardLibrary.ElanArray<string> contents => StandardLibrary.ElanArray<string>.DefaultInstance;
+      protected override int rear => default;
+      protected override int maxSize => default;
+      public override void initialise() { }
+      public override void withdrawNextLetter(ref string letter) { }
+      public override void add() { }
+      public override string asString() { return ""default QueueOfTiles"";  }
+    }
+  }
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+    var q = new QueueOfTiles(10);
+    printLine(q.show());
+  }
+}";
+
+        var parseTree = @"*";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "xxx");
 
     }
     #endregion
