e08716b09b975b703aac98aa4cde077da223a10e
Richard Pawson <rpawson@nakedobjects.net>
2023-11-07 12:41:43 +0000

    #93 test written (1 Ignored)

---
+++ Test/CompilerTests/T93_PassingLoopVariableIntoProcedure.cs
@@ -0,0 +1,114 @@
+﻿using Compiler;
+
+namespace Test.CompilerTests;
+
+using static Helpers;
+
+[TestClass]
+public class T93_PassingLoopVariableIntoProcedure
+{
+    #region Passes
+
+
+    [TestMethod]
+    public void Pass_CorrectedPattern()
+    {
+        var code = @"#
+procedure removeLetters(wordAsPlayed String)
+  foreach letter in wordAsPlayed
+    var x = letter
+    removeLetter(x)
+  end foreach
+end procedure
+
+procedure removeLetter(l Char)
+end procedure
+
+main
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static Globals;
+using static StandardLibrary.SystemCalls;
+using static StandardLibrary.Constants;
+
+public static partial class Globals {
+  public static void removeLetters(ref string wordAsPlayed) {
+    foreach (var letter in wordAsPlayed) {
+      var x = letter;
+      removeLetter(ref x);
+    }
+  }
+  public static void removeLetter(ref char l) {
+
+  }
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+
+  }
+}";
+
+        var parseTree = @"*";
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+    }
+
+    #endregion
+
+    #region Fails
+    [TestMethod, Ignore]
+    public void Fail_InvalidPattern()
+    {
+        var code = @"#
+procedure removeLetters(wordAsPlayed String)
+  foreach letter in wordAsPlayed
+    removeLetter(letter)
+  end foreach
+end procedure
+
+procedure removeLetter(l Char)
+end procedure
+
+main
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using System.Collections.Immutable;
+using static Globals;
+using static StandardLibrary.SystemCalls;
+using static StandardLibrary.Constants;
+
+public static partial class Globals {
+  public static void removeLetters(ref string wordAsPlayed) {
+    foreach (var letter in wordAsPlayed) {
+      removeLetter(ref letter);
+    }
+  }
+  public static void removeLetter(ref char l) {
+
+  }
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+
+  }
+}";
+
+        var parseTree = @"*";
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertDoesNotCompile(compileData, "Cannot pass control variable into a procedure (consider declaring a new variable copying the value)");
+    }
+    #endregion
+}
