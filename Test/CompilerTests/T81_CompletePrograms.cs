1295e9e6d546dab034bbee87d105f0b52deb02ac
Richard Pawson <rpawson@nakedobjects.net>
2023-11-07 14:26:27 +0000

    #81 More comprehensive testing of methods on two classes

--- Test/CompilerTests/T81_CompletePrograms.cs
+++ Test/CompilerTests/T81_CompletePrograms.cs
@@ -155,96 +155,7 @@ public static class Program {
     {
         var code = ReadElanSourceCodeFile("words_QueueOfTiles.elan");
 
-        var objectCode = @"using System.Collections.Generic;
-using System.Collections.Immutable;
-using static Globals;
-using static StandardLibrary.SystemCalls;
-using static StandardLibrary.Constants;
-
-public static partial class Globals {
-  public const int startHandSize = 15;
-  public const int maxHandSize = 20;
-  public const int maxTilesPlayed = 50;
-  public const string letters = @$""****ABCDEFGHIJKLMNOPQRSTUVWXYZ"";
-  public const string mainMenu = @$""
-========= 
-MAIN MENU
-=========
-
-1. Play game with random start hand,
-2. Play game with training start hand
-9. Quit"";
-  public static readonly StandardLibrary.ElanDictionary<char,int> tileDictionary = new StandardLibrary.ElanDictionary<char,int>(('A', 1), ('B', 2), ('C', 2), ('D', 2), ('E', 1), ('F', 3), ('G', 2), ('H', 3), ('I', 1), ('J', 5), ('K', 3), ('L', 2), ('M', 2), ('N', 1), ('O', 1), ('P', 2), ('Q', 5), ('R', 1), ('S', 1), ('T', 1), ('U', 2), ('V', 3), ('W', 3), ('X', 5), ('Y', 3), ('Z', 5));
-  public record class QueueOfTiles {
-    public static QueueOfTiles DefaultInstance { get; } = new QueueOfTiles._DefaultQueueOfTiles();
-    private QueueOfTiles() {}
-    public QueueOfTiles(int maxSize) {
-      this.maxSize = maxSize;
-      contents = new StandardLibrary.ElanArray<string>(maxSize);
-      rear = -1;
-    }
-    protected virtual StandardLibrary.ElanArray<string> contents { get; set; } = StandardLibrary.ElanArray<string>.DefaultInstance;
-    protected virtual int rear { get; set; } = default;
-    protected virtual int maxSize { get; set; } = default;
-    public virtual Boolean isNotEmpty() {
-
-      return rear != -1;
-    }
-    public virtual string show() {
-      var result = @$"""";
-      foreach (var letter in contents) {
-        result = result + letter;
-      }
-      return result + newline;
-    }
-    public virtual string asString() {
-
-      return StandardLibrary.Functions.typeAndProperties(this);
-    }
-    public virtual void initialise() {
-      rear = -1;
-      for (var count = 0; count <= maxSize - 2; count = count + 1) {
-        add();
-      }
-    }
-    public virtual void withdrawNextLetter(ref string letter) {
-      if (isNotEmpty()) {
-        letter = contents[0];
-        for (var count = 1; count <= rear; count = count + 1) {
-          contents[count - 1] = contents[count];
-          contents[rear] = @$"""";
-        }
-        rear = rear - 1;
-      }
-    }
-    public virtual void add() {
-      if (rear < maxSize - 1) {
-        rear = rear + 1;
-        var n = random(0, 30);
-        contents[rear] = StandardLibrary.Functions.asString(letters[n]);
-      }
-    }
-    private record class _DefaultQueueOfTiles : QueueOfTiles {
-      public _DefaultQueueOfTiles() { }
-      protected override StandardLibrary.ElanArray<string> contents => StandardLibrary.ElanArray<string>.DefaultInstance;
-      protected override int rear => default;
-      protected override int maxSize => default;
-      public override void initialise() { }
-      public override void withdrawNextLetter(ref string letter) { }
-      public override void add() { }
-      public override string asString() { return ""default QueueOfTiles"";  }
-    }
-  }
-}
-
-public static class Program {
-  private static void Main(string[] args) {
-    seedRandom(1);
-    var q = new QueueOfTiles(10);
-    q.initialise();
-    printLine(q.show());
-  }
-}";
+        //var objectCode = @"";
 
         var parseTree = @"*";
 
@@ -254,81 +165,14 @@ public static class Program {
         AssertCompiles(compileData);
         //AssertObjectCodeIs(compileData, objectCode);
         AssertObjectCodeCompiles(compileData);
-        AssertObjectCodeExecutes(compileData, "EALUQJHZAQ\r\n\r\n");
-
+        AssertObjectCodeExecutes(compileData, "EALUQJHZAQ\r\n\r\ntrue\r\nE\r\nALUQJHZAQ\r\n\r\n");
     }
 
     [TestMethod]
     public void Pass_Player()
     {
         var code = ReadElanSourceCodeFile("words_Player.elan");
-        var objectCode = @"using System.Collections.Generic;
-using System.Collections.Immutable;
-using static Globals;
-using static StandardLibrary.SystemCalls;
-using static StandardLibrary.Constants;
-
-public static partial class Globals {
-  public static readonly StandardLibrary.ElanDictionary<char,int> tileDictionary = new StandardLibrary.ElanDictionary<char,int>(('A', 1), ('B', 2), ('C', 2), ('D', 2), ('E', 1), ('F', 3), ('G', 2), ('H', 3), ('I', 1), ('J', 5), ('K', 3), ('L', 2), ('M', 2), ('N', 1), ('O', 1), ('P', 2), ('Q', 5), ('R', 1), ('S', 1), ('T', 1), ('U', 2), ('V', 3), ('W', 3), ('X', 5), ('Y', 3), ('Z', 5));
-  public record class Player {
-    public static Player DefaultInstance { get; } = new Player._DefaultPlayer();
-    private Player() {}
-    public Player(string name) {
-      this.name = name;
-      score = 50;
-    }
-    public virtual string name { get; set; } = """";
-    public virtual int score { get; set; } = default;
-    public virtual int tilesPlayed { get; set; } = default;
-    public virtual string tilesInHand { get; set; } = """";
-    public virtual string asString() {
-
-      return name;
-    }
-    public virtual void setTilesInHand(ref string tiles) {
-      tilesInHand = tiles;
-    }
-    public virtual void addToLetters(ref char letter) {
-      tilesInHand = tilesInHand + letter;
-    }
-    public virtual void addToScore(ref int points) {
-      score = score + points;
-    }
-    public virtual void removeLetter(ref char letter) {
-      var i = StandardLibrary.Functions.indexOf(tilesInHand, letter);
-      tilesInHand = tilesInHand[..(i - 1)] + tilesInHand[(i + 1)..];
-    }
-    public virtual void removeLetters(ref string wordAsPlayed) {
-      foreach (var letter in wordAsPlayed) {
-        removeLetter(ref letter);
-      }
-    }
-    public virtual void deductAnyPenalty() {
-      var x = 0;
-    }
-    private record class _DefaultPlayer : Player {
-      public _DefaultPlayer() { }
-      public override string name => """";
-      public override int score => default;
-      public override int tilesPlayed => default;
-      public override string tilesInHand => """";
-      public override void setTilesInHand(ref string tiles) { }
-      public override void addToLetters(ref char letter) { }
-      public override void addToScore(ref int points) { }
-      public override void removeLetter(ref char letter) { }
-      public override void removeLetters(ref string wordAsPlayed) { }
-      public override void deductAnyPenalty() { }
-      public override string asString() { return ""default Player"";  }
-    }
-  }
-}
-
-public static class Program {
-  private static void Main(string[] args) {
-    var p = new Player(@$""Richard"");
-    printLine(p.name);
-  }
-}";
+        //var objectCode = @"";
         var parseTree = @"*";
         var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
         AssertParses(compileData);
@@ -336,7 +180,7 @@ public static class Program {
         AssertCompiles(compileData);
         //AssertObjectCodeIs(compileData, objectCode);
         AssertObjectCodeCompiles(compileData);
-        AssertObjectCodeExecutes(compileData, "Richard\r\n");
+        AssertObjectCodeExecutes(compileData, "Richard\r\nABCDEIOU\r\nABCDEIOUN\r\nABCDEIUN\r\nABCEIN\r\nCAB\r\nCA*\r\n50\r\n42\r\n");
     }
     #endregion
 
