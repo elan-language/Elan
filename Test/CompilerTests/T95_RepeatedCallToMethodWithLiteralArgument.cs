f531a530b9d2026cd694f59d8ec8184c1dcaf775
Richard Pawson <rpawson@nakedobjects.net>
2023-11-07 13:58:45 +0000

    #95 test written

---
+++ Test/CompilerTests/T95_RepeatedCallToMethodWithLiteralArgument.cs
@@ -0,0 +1,43 @@
+﻿using Compiler;
+
+namespace Test.CompilerTests;
+
+using static Helpers;
+
+[TestClass]
+public class T95_RepeatedCallToProcedureWithLiteralArgument
+{
+    #region Passes
+
+    [TestMethod]
+    public void Pass_Template() {
+        var code = @"
+main
+  square(3)
+  square(5)
+end main
+
+procedure square(x Int)
+  printLine(x * x)
+end procedure
+";
+
+        var objectCode = @"";
+
+        var parseTree = @"*";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "9\r\n25\r\n");
+    }
+
+    #endregion
+
+    #region Fails
+
+    #endregion
+}
