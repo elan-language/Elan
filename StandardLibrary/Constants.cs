3d0dafbc715dd091055cb54d74c0a3d0f5693141
Richard Pawson <rpawson@nakedobjects.net>
2023-11-08 11:22:38 +0000

    #96 Tow tests, one ignored

--- StandardLibrary/Constants.cs
+++ StandardLibrary/Constants.cs
@@ -6,4 +6,32 @@ public static class Constants {
 
     public static string newline => @"
 ";
+
+    public enum Direction
+    {
+        left,
+        right,
+        up,
+        down
+    }
+
+    public enum Colour
+    {
+        black = 0,
+        darkBlue = 1,
+        darkGreen = 2,
+        darkCyan = 3,
+        darkRed = 4,
+        darkMagenta = 5,
+        darkYellow = 6,
+        grey = 7,
+        darkGrey = 8,
+        blue = 9,
+        green = 10,
+        cyan = 11,
+        red = 12,
+        magenta = 13,
+        yellow = 14,
+        white = 15
+    }
 }
--- StandardLibrary/Types.cs
+++ StandardLibrary/Types.cs
@@ -1,30 +1,5 @@
 ﻿namespace StandardLibrary;
 
-public enum Direction {
-    left,
-    right,
-    up,
-    down
-}
-
-public enum Colour {
-    black = 0,
-    darkBlue = 1,
-    darkGreen = 2,
-    darkCyan = 3,
-    darkRed = 4,
-    darkMagenta = 5,
-    darkYellow = 6,
-    grey = 7,
-    darkGrey = 8,
-    blue = 9,
-    green = 10,
-    cyan = 11,
-    red = 12,
-    magenta = 13,
-    yellow = 14,
-    white = 15
-}
 
 public class StopWatch { }
 
