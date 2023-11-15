acf42556edfa48598ad55e58233b44355f2733d5
Richard Pawson <rpawson@nakedobjects.net>
2023-11-15 10:55:34 +0000

    #112 additional test

--- Test/CompilerTests/T_2_HelloWorld.cs
+++ Test/CompilerTests/T_2_HelloWorld.cs
@@ -90,6 +90,42 @@ public static class Program {
     }
 
     [TestMethod]
+    public void Pass_BracketsMakeNoDifference()
+    {
+        var code = @"#
+main
+  print(""Hello World!"")
+end main
+";
+
+        var objectCode = @"using System.Collections.Generic;
+using StandardLibrary;
+using static Globals;
+using static StandardLibrary.SystemAccessors;
+using static StandardLibrary.Procedures;
+using static StandardLibrary.Constants;
+
+public static partial class Globals {
+
+}
+
+public static class Program {
+  private static void Main(string[] args) {
+    System.Console.WriteLine(StandardLibrary.Functions.asString((@$""Hello World!"")));
+  }
+}";
+
+        var parseTree = @"*";
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "Hello World!\r\n");
+    }
+
+    [TestMethod]
     public void Pass_IntegerLiteral()
     {
         var code = @"#
