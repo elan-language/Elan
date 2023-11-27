538d0e25b1b3eaac71281b5b27308cc6d2badc74
Stefano Cascarini <stef.cascarini@googlemail.com>
2023-11-27 11:35:06 +0000

    #26 fix iter range

---
+++ StandardLibrary/ElanString.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace StandardLibrary;
+
+public class ElanString : ElanIter<char> {
+    private readonly string wrappedString;
+
+    public ElanString(string wrappedString) => this.wrappedString = wrappedString;
+    public IEnumerator<char> GetEnumerator() => wrappedString.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public char this[int index] => wrappedString[index];
+
+    public ElanIter<char> this[Range range] => new ElanString(wrappedString[range]);
+
+    public static implicit operator string(ElanString es) => es.wrappedString;
+
+    public static implicit operator ElanString(string s) => new(s);
+}
