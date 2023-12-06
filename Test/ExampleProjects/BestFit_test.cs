385f5ecaa5fab5db9f5081af7f13c73ead8bbc72
Richard Pawson <rpawson@nakedobjects.net>
2023-12-06 17:22:40 +0000

    #145 & #150  - tests written in Elan

---
+++ Test/ExampleProjects/BestFit_test.cs
@@ -0,0 +1,35 @@
+﻿using Compiler;
+using Test.CompilerTests;
+
+namespace Test.ExampleProjects;
+
+using static Helpers;
+
+[TestClass]
+public class BestFit_test
+{
+    #region Passes
+
+    [TestMethod]
+    public void Pass_BinarySearch()
+    {
+        var code = ReadCodeFile("BestFit.elan"); ;
+        var objectCode = ReadCodeFile("BestFit.obj");
+
+        var parseTree = @"*";
+
+        var compileData = Pipeline.Compile(new CompileData { ElanCode = code });
+        AssertParses(compileData);
+        AssertParseTreeIs(compileData, parseTree);
+        AssertCompiles(compileData);
+        AssertObjectCodeIs(compileData, objectCode);
+        AssertObjectCodeCompiles(compileData);
+        AssertObjectCodeExecutes(compileData, "\r\n");
+    }
+
+    #endregion
+
+    #region Fails
+
+    #endregion
+}
