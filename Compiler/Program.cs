d90bf8ff405e4b0f4ac9153b88099ffe78349674
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-01-17 11:36:10 +0000

    Output compile errors

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -2,8 +2,21 @@
 
 public static class Program {
     private static void HandleCompile(string fileName) {
-        Pipeline.Compile(fileName);
-        Console.WriteLine("Compiled OK");
+        var compileData = Pipeline.Compile(fileName);
+
+        if (compileData.ParserErrors.Length > 0 || compileData.CompileErrors.Length > 0) {
+            Console.WriteLine("Failed to Compile");
+            foreach (var error in compileData.ParserErrors) {
+                Console.WriteLine(error.Message);
+            }
+
+            foreach (var error in compileData.CompileErrors) {
+                Console.WriteLine(error);
+            }
+        }
+        else {
+            Console.WriteLine("Compiled OK");
+        }
     }
 
     private static void Main(string[] args) {
