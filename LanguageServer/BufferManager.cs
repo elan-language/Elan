27ec7cfe81bf843f86c13db49b8a4975b6acb4ac
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-02-12 14:29:13 +0000

    IDE #112 create language server

---
+++ LanguageServer/BufferManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+
+namespace Server
+{
+    internal class BufferManager
+    {
+        private ConcurrentDictionary<string, string> _buffers = new ConcurrentDictionary<string, string>();
+
+        public void UpdateBuffer(string documentPath, string buffer)
+        {
+            _buffers.AddOrUpdate(documentPath, buffer, (k, v) => buffer);
+        }
+
+        public string GetBuffer(string documentPath)
+        {
+            return _buffers.TryGetValue(documentPath, out var buffer) ? buffer : null;
+        }
+    }
+}
---
+++ LanguageServer/TextDocumentSyncHandler.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Window;
+
+namespace Server
+{
+    internal class TextDocumentSyncHandler : ITextDocumentSyncHandler
+    {
+        private readonly ILanguageServer _router;
+        private readonly BufferManager _bufferManager;
+
+        private readonly TextDocumentSelector _documentSelector = new TextDocumentSelector(
+            new TextDocumentFilter()
+            {
+                Pattern = "**/*.csproj"
+            }
+        );
+
+        private TextSynchronizationCapability _capability;
+
+        public TextDocumentSyncHandler(ILanguageServer router, BufferManager bufferManager)
+        {
+            _router = router;
+            _bufferManager = bufferManager;
+        }
+
+        public TextDocumentSyncKind Change { get; } = TextDocumentSyncKind.Full;
+
+        public TextDocumentChangeRegistrationOptions GetRegistrationOptions()
+        {
+            return new TextDocumentChangeRegistrationOptions()
+            {
+                DocumentSelector = _documentSelector,
+                SyncKind = Change
+            };
+        }
+
+        public TextDocumentAttributes GetTextDocumentAttributes(Uri uri)
+        {
+            return new TextDocumentAttributes(uri, "xml");
+        }
+
+        public Task<Unit> Handle(DidChangeTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            var documentPath = request.TextDocument.Uri.ToString();
+            var text = request.ContentChanges.FirstOrDefault()?.Text;
+
+            _bufferManager.UpdateBuffer(documentPath, text);
+
+            _router.Window.LogInfo($"Updated buffer for document: {documentPath}\n{text}");
+
+            return Unit.Task;
+        }
+
+        public Task<Unit> Handle(DidOpenTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            _bufferManager.UpdateBuffer(request.TextDocument.Uri.ToString(), request.TextDocument.Text);
+            return Unit.Task;
+        }
+
+        public Task<Unit> Handle(DidCloseTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            return Unit.Task;
+        }
+
+        public Task<Unit> Handle(DidSaveTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            return Unit.Task;
+        }
+
+        public void SetCapability(TextSynchronizationCapability capability)
+        {
+            _capability = capability;
+        }
+
+        //ITextDocumentRegistrationOptions IRegistration<TextDocumentRegistrationOptions>.GetRegistrationOptions()
+        //{
+        //    return new TextDocumentSaveRegistrationOptions()
+        //    {
+        //        DocumentSelector = _documentSelector,
+        //    };
+        //}
+
+        //TextDocumentSaveRegistrationOptions IRegistration<TextDocumentSaveRegistrationOptions>.GetRegistrationOptions()
+        //{
+        //    return new TextDocumentSaveRegistrationOptions()
+        //    {
+        //        DocumentSelector = _documentSelector,
+        //        IncludeText = true
+        //    };
+        //}
+        TextDocumentChangeRegistrationOptions IRegistration<TextDocumentChangeRegistrationOptions, TextSynchronizationCapability>.GetRegistrationOptions(TextSynchronizationCapability capability, ClientCapabilities clientCapabilities) {
+            throw new NotImplementedException();
+        }
+
+        TextDocumentOpenRegistrationOptions IRegistration<TextDocumentOpenRegistrationOptions, TextSynchronizationCapability>.GetRegistrationOptions(TextSynchronizationCapability capability, ClientCapabilities clientCapabilities) {
+            throw new NotImplementedException();
+        }
+
+        TextDocumentCloseRegistrationOptions IRegistration<TextDocumentCloseRegistrationOptions, TextSynchronizationCapability>.GetRegistrationOptions(TextSynchronizationCapability capability, ClientCapabilities clientCapabilities) {
+            throw new NotImplementedException();
+        }
+
+        TextDocumentSaveRegistrationOptions IRegistration<TextDocumentSaveRegistrationOptions, TextSynchronizationCapability>.GetRegistrationOptions(TextSynchronizationCapability capability, ClientCapabilities clientCapabilities) {
+            return new TextDocumentSaveRegistrationOptions()
+            {
+                DocumentSelector = _documentSelector,
+                IncludeText = true
+            };
+        }
+
+        public TextDocumentAttributes GetTextDocumentAttributes(DocumentUri uri) {
+            throw new NotImplementedException();
+        }
+    }
+}
